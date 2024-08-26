1cadde30a369c350b6192421724db2a4e47a344e
AzizjonMukhtorov <mukhtorovsino13@gmail.com>
2024-08-26 20:00:01 +0500

    Task2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-def say_hello():
-    return "Say Hello"
+# def say_hello():
+#     return "Say Hello"
 
-print(say_hello())
+# print(say_hello())
 
-def say_goodbye():
-    return "Goodbye"
+# def say_goodbye():
+#     return "Goodbye"
 
-def say_say():
-    return 1
+# def say_say():
+#     return 1
+
+# print("1")
 
 
 
